a692abb458a647b03880a4ea55fcf510a0676d30
Miroslav-IvanovV <miroslavivanov002@gmail.com>
2021-08-03 07:01:26 +0300

    third commit

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -25,6 +25,7 @@ public class Car {
         this.consumptions = consumptions;
     }
 
+    //showing the characteristics of the car
     public String carCharacteristics(){
         return "Doors : " + door + "\n" + "Seats : " + seats + "\n" + "Color : " + color + "\n" + "Creation year : "
                 + creationYear + "\n" + "Steering Wheel : " + steeringWheel + "\n" + "Gear shift : " + gearShift + "\n"
@@ -33,11 +34,45 @@ public class Car {
 
     }
 
+    //Getter
+    public int getCurrentShift() {
+        return currentShift;
+    }
+
+    //comparing the color of hte car with the input color
     public boolean isItTheSameColor(String color){
         return this.color == color;
     }
 
+    // checking if the shift is right
     public boolean isTheShiftRight(int shift){
         return currentShift == shift;
+    }
+
+    // increasing current shift by one
+    public void increasingCurrentShift(){
+        this.currentShift++;
+    }
+
+    // decreasing current shift by one
+    public void decreasingCurrentShift(){
+        this.currentShift--;
+    }
+
+    //checking if there is enough space for the passengers
+    public boolean areTheSeatsEnough(int seats){
+        if(this.seats - seats > 0) return true;
+        else return false;
+    }
+
+    // checking if the passengers are too heavy
+    public boolean arePassengersAndBagsTooHeavy(int passengers, int bags){
+        if(passengers * 60 + bags * 40 > 3500) return true;
+        else return false;
+    }
+
+    //how much fuel do we need to get to the destination
+    public double howMuchFuel(int destination){
+        return consumptions * destination;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        
         Car car1 = new Car(1,1,"red",1999, "left", "manuel",2,
                 1000, 6.6);
 
+        //checking if the methods are working correctly
         System.out.println(car1.carCharacteristics());
-        System.out.println(car1.isItTheSameColor("red"));
+
+        System.out.println("is the car the same color as the input " + car1.isItTheSameColor("red"));
+
+        System.out.println("is it the same gear as the input " + car1.isTheShiftRight(3));
+
+        car1.increasingCurrentShift();
+        System.out.println(car1.getCurrentShift());
+
+        car1.decreasingCurrentShift();
+        System.out.println(car1.getCurrentShift());
+
+        System.out.println("is there enough space " + car1.areTheSeatsEnough(2));
+
+        System.out.println("are the passengers and bags too heavy " + car1.arePassengersAndBagsTooHeavy(2, 5));
+
+        System.out.println("you need " + car1.howMuchFuel(300) + " liters to reach your destination");
     }
 }
